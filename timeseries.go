f36626d8e9cc0294e83b5b7e368b7e102ebf0e98
Sölvi Páll Ásgeirsson <solvip@gmail.com>
2018-02-19 23:12:30 +0000

    refactor Timeseries to better match gonum semantics

--- timeseries.go
+++ timeseries.go
@@ -1,29 +1,31 @@
-// Package timeseries provides utilities to manipulate and analyze timeseries data
-// For ease of use by clients, the Timeseries is simply a slice of points.
-// This, however, means that you might need to call Sort() if you're not appending
-// points in sorted order.
+// Package timeseries provides utilities to manipulate and analyze timeseries data.
+// For compatability with Gonum, a Timeseries is simply a pair of float64 slices,
+// the X and the Y axis.
+// You can manipulate them as you wish, but ensure that you call Sort() if you're
+// not appending data points in sorted order.
 package timeseries
 
 import (
 	"sort"
-	"time"
 )
 
-type Timeseries []Point
-
-type Point struct {
-	Time  time.Time
-	Value float64
+type Timeseries struct {
+	Xs []float64
+	Ys []float64
 }
 
 // Equal - Return true if t and other represent the same time series
 func (t Timeseries) Equal(other Timeseries) bool {
-	if len(t) != len(other) {
+	if len(t.Xs) != len(t.Ys) || len(other.Xs) != len(other.Ys) {
+		panic("timeseries: Xs and Ys length mismatch")
+	}
+
+	if t.Len() != other.Len() {
 		return false
 	}
 
-	for i := range t {
-		if t[i] != other[i] {
+	for i := 0; i < t.Len(); i++ {
+		if t.Xs[i] != other.Xs[i] || t.Ys[i] != other.Ys[i] {
 			return false
 		}
 	}
@@ -33,71 +35,128 @@ func (t Timeseries) Equal(other Timeseries) bool {
 
 // After - Return a shallow copy of the items in the time series having Time >= instant
 // The series must be sorted.
-func (t Timeseries) After(instant time.Time) Timeseries {
-	if i := t.findPivot(instant); i == len(t) {
+func (t Timeseries) After(y float64) Timeseries {
+	if len(t.Xs) != len(t.Ys) {
+		panic("timeseries: Xs and Ys length mismatch")
+	}
+
+	if i := t.findPivot(y); i == t.Len() {
 		// After is older than all the items in the series
 		return Timeseries{}
 	} else {
-		return t[i:]
+		return Timeseries{
+			Xs: t.Xs[i:],
+			Ys: t.Ys[i:],
+		}
 	}
 }
 
 // Before - Return a shallow copy of the items in the time series having Time < instant.
 // The series must be sorted.
-func (t Timeseries) Before(instant time.Time) Timeseries {
-	j := t.findPivot(instant)
-	return t[:j]
-}
-
-// Between - Return a shallow copy of the items in the time series between [from, to)
-func (t Timeseries) Between(from, to time.Time) Timeseries {
-	return t.After(from).Before(to)
-}
-
-// findPivot - Binary search for the location of instant in t and return its index,
-// where the index is before <= instant < after
-func (t Timeseries) findPivot(instant time.Time) int {
-	findAfter := func(i int) bool {
-		return t[i].Time.Equal(instant) || t[i].Time.After(instant)
+func (t Timeseries) Before(y float64) Timeseries {
+	if len(t.Xs) != len(t.Ys) {
+		panic("timeseries: Xs and Ys length mismatch")
 	}
 
-	return sort.Search(len(t), findAfter)
+	if j := t.findPivot(y); j == t.Len() {
+		return t
+	} else {
+		return Timeseries{
+			Xs: t.Xs[:j],
+			Ys: t.Ys[:j],
+		}
+	}
 }
 
-// Append - Append the Point p to the timeseries
-// Note that you might need a sort if you're inserting points out-of-order
-func (t *Timeseries) AppendPoint(p Point) {
-	*t = append(*t, p)
+// Between - Return a shallow copy of the items in the time series between [y1, y2)
+func (t Timeseries) Between(y1, y2 float64) Timeseries {
+	if len(t.Xs) != len(t.Ys) {
+		panic("timeseries: Xs and Ys length mismatch")
+	}
+
+	return t.After(y1).Before(y2)
+}
+
+// findPivot - Binary search for the location of y in t and return its index,
+// where the index will put i at before <= y < after
+func (t Timeseries) findPivot(y float64) int {
+	findAfter := func(i int) bool {
+		return t.Ys[i] >= y
+	}
+
+	return sort.Search(t.Len(), findAfter)
 }
 
 // Append - Append value @ time to the timeseries
 // Note that you might need a sort if you're inserting points out-of-order
-func (t *Timeseries) Append(time time.Time, value float64) {
-	t.AppendPoint(Point{Time: time, Value: value})
+func (t *Timeseries) Append(x float64, y float64) {
+	if len(t.Xs) != len(t.Ys) {
+		panic("timeseries: Xs and Ys slice length mismatch")
+	}
+
+	t.Xs = append(t.Xs, x)
+	t.Ys = append(t.Ys, y)
 }
 
 // Difference the timeseries N, returning a new series of length len(N)-1
 func (t Timeseries) Difference() (ret Timeseries) {
+	if len(t.Xs) != len(t.Ys) {
+		panic("timeseries: Xs and Ys slice length mismatch")
+	}
+
 	if t.Len() < 2 {
 		// We must have at least two elements to difference
 		return ret
 	}
 
-	ret = make(Timeseries, len(t)-1)
-	for i := range ret {
-		ret[i] = Point{
-			Time:  t[i+1].Time,
-			Value: t[i+1].Value - t[i].Value,
-		}
+	ret = makeTimeseries(t.Len() - 1)
+	for i := 0; i < ret.Len(); i++ {
+		ret.Xs[i] = t.Xs[i+1] - t.Xs[i]
+		ret.Ys[i] = t.Ys[i+1]
 	}
 
 	return ret
 }
 
+// Slice slices the Timeseries equivalently to t[start:end]
+func (t Timeseries) Slice(start, end int) Timeseries {
+	if len(t.Xs) != len(t.Ys) {
+		panic("timeseries: Xs and Ys slice length mismatch")
+	}
+
+	return Timeseries{
+		Xs: t.Xs[start:end],
+		Ys: t.Ys[start:end],
+	}
+}
+
+func makeTimeseries(length int) Timeseries {
+	return Timeseries{
+		Xs: make([]float64, length),
+		Ys: make([]float64, length),
+	}
+}
+
 func (t Timeseries) Sort() {
+	if len(t.Xs) != len(t.Ys) {
+		panic("timeseries: Xs and Ys slice length mismatch")
+	}
+
 	sort.Sort(t)
 }
 
-func (t Timeseries) Len() int           { return len(t) }
-func (t Timeseries) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
-func (t Timeseries) Less(i, j int) bool { return t[i].Time.Before(t[j].Time) }
+func (t Timeseries) Len() int {
+	if n := len(t.Xs); n != len(t.Ys) {
+		panic("timeseries: Xs and Ys slice length mismatch")
+	} else {
+		return n
+	}
+}
+
+func (t Timeseries) Swap(i, j int) {
+	t.Xs[i], t.Xs[j] = t.Xs[j], t.Xs[i]
+	t.Ys[i], t.Ys[j] = t.Ys[j], t.Ys[i]
+}
+func (t Timeseries) Less(i, j int) bool {
+	return t.Ys[i] < t.Ys[j]
+}
--- timeseries_test.go
+++ timeseries_test.go
@@ -1,23 +1,23 @@
 package timeseries
 
 import (
+	"math"
 	"testing"
-	"time"
 )
 
 var emptyTimeseries = Timeseries{}
-var zeroTime = time.Time{}
-var maxTime = time.Unix(1<<63-62135596801, 999999999)
+var minY = math.Inf(-1)
+var maxY = math.Inf(1)
 
 func TestAppend(t *testing.T) {
 	expected := Timeseries{
-		Point{Time: time.Now(), Value: 123.4},
-		Point{Time: time.Now(), Value: 456.7},
+		Xs: []float64{123.4, 456.7},
+		Ys: []float64{1, 2},
 	}
 
 	var actual Timeseries
-	actual.Append(expected[0].Time, expected[0].Value)
-	actual.AppendPoint(expected[1])
+	actual.Append(expected.Xs[0], expected.Ys[0])
+	actual.Append(expected.Xs[1], expected.Ys[1])
 
 	if !actual.Equal(expected) {
 		t.Fatalf("expected %v after appending; instead got %v", expected, actual)
@@ -26,76 +26,77 @@ func TestAppend(t *testing.T) {
 
 func TestAfter(t *testing.T) {
 	ts := Timeseries{
-		{time.Date(2018, 2, 15, 22, 41, 31, 0, time.UTC), 100.0},
-		{time.Date(2018, 2, 15, 22, 41, 41, 0, time.UTC), 50.0},
-		{time.Date(2018, 2, 15, 22, 41, 51, 0, time.UTC), 100.0},
+		Xs: []float64{100.0, 50.0, 100.0},
+		Ys: []float64{1, 2, 3},
 	}
 
-	if x := emptyTimeseries.After(zeroTime); !x.Equal(emptyTimeseries) {
-		t.Fatalf("expected empty series to be returned on emptyTimeseries.After(zeroTime); instead got %v", x)
+	if x := emptyTimeseries.After(minY); !x.Equal(emptyTimeseries) {
+		t.Fatalf("expected emptyTimeseries.After(minY) to return empty series; instead got %v", x)
 	}
 
-	if x := ts.After(maxTime); !x.Equal(emptyTimeseries) {
-		t.Fatalf("expected no items in time series after maximum time, instead got: %v", x)
+	if x := ts.After(maxY); !x.Equal(emptyTimeseries) {
+		t.Fatalf("expected no items in time series after maximum y, instead got: %v", x)
 	}
 
-	if x := ts.After(zeroTime); !ts.Equal(ts) {
-		t.Fatalf("expected ts.After(zeroTime) to return ts; instead got: %v", x)
+	if x := ts.After(0); !ts.Equal(ts) {
+		t.Fatalf("expected ts.After(0) to return ts; instead got: %v", x)
 	}
 
 	// After is inclusive
-	if x := ts.After(ts[1].Time); !x.Equal(ts[1:]) {
-		t.Fatalf("expected ts.After(ts[1].Time) to return %v; instead got %v", ts[1:], x)
+	expected := ts.Slice(1, ts.Len())
+	if x := ts.After(2); !x.Equal(expected) {
+		t.Fatalf("expected ts.After(2) to return %v; instead got %v", expected, x)
 	}
 }
 
 func TestBefore(t *testing.T) {
 	ts := Timeseries{
-		{time.Date(2018, 2, 15, 22, 41, 31, 0, time.UTC), 100.0},
-		{time.Date(2018, 2, 15, 22, 41, 41, 0, time.UTC), 50.0},
-		{time.Date(2018, 2, 15, 22, 41, 51, 0, time.UTC), 100.0},
+		Xs: []float64{100.0, 50.0, 100.0},
+		Ys: []float64{1, 2, 3},
 	}
 
-	if x := emptyTimeseries.Before(maxTime); !x.Equal(emptyTimeseries) {
+	if x := emptyTimeseries.Before(math.Inf(-1)); !x.Equal(emptyTimeseries) {
 		t.Fatalf("expected nil to be returned on emptyTimeseries.Before(maxTime); instead got %v", x)
 	}
 
-	if x := ts.Before(zeroTime); !x.Equal(emptyTimeseries) {
+	if x := ts.Before(minY); !x.Equal(emptyTimeseries) {
 		t.Fatalf("expected no items in time series before zero time, instead got %v", x)
 	}
 
-	if x := ts.Before(maxTime); !x.Equal(ts) {
+	if x := ts.Before(maxY); !x.Equal(ts) {
 		t.Fatalf("expected ts.Before(maxTime) to return ts; instead got: %v", x)
 	}
 
 	// Before is exclusive
-	if x := ts.Before(ts[2].Time); !x.Equal(ts[:2]) {
-		t.Fatalf("expected ts.Before(%s) to return %v; instead got %v", ts[2].Time, ts[:2], x)
+	expected := ts.Slice(0, 2)
+	if x := ts.Before(ts.Ys[2]); !x.Equal(expected) {
+		t.Fatalf("expected ts.Before(%v) to return %v; instead got %v", ts.Ys[2], expected, x)
 	}
 }
 
 func TestBetween(t *testing.T) {
 	ts := Timeseries{
-		{time.Date(2018, 2, 15, 22, 41, 31, 0, time.UTC), 100.0},
-		{time.Date(2018, 2, 15, 22, 41, 41, 0, time.UTC), 50.0},
-		{time.Date(2018, 2, 15, 22, 41, 51, 0, time.UTC), 100.0},
+		Xs: []float64{100.0, 50.0, 100.0},
+		Ys: []float64{1, 2, 3},
 	}
 
-	if x := ts.Between(zeroTime, zeroTime); !x.Equal(emptyTimeseries) {
-		t.Fatalf("expected ts.Between(zeroTime, zeroTime) to return empty; instead got %v", x)
+	if x := ts.Between(minY, minY); !x.Equal(emptyTimeseries) {
+		t.Fatalf("expected ts.Between(minY, minY) to return empty; instead got %v", x)
 	}
 
 	// We return in the range [from, to).  We should only get the first two elements.
-	if x := ts.Between(zeroTime, ts[2].Time); !x.Equal(ts[0:2]) {
-		t.Fatalf("expected ts.Between(zeroTime, %s) to return %v; instead got %v", ts[2].Time, ts[0:2], x)
+	firstTwo := ts.Slice(0, 2)
+	if x := ts.Between(minY, ts.Ys[2]); !x.Equal(firstTwo) {
+		t.Fatalf("expected ts.Between(minY, %v) to return %v; instead got %v", ts.Ys[2], firstTwo, x)
 	}
 
 	// we return in the range [from, to).  Ensure that we only get the middle
 	// element
-	a := ts[1].Time
-	b := ts[2].Time
-	if x := ts.Between(a, b); !x.Equal(ts[1:2]) {
-		t.Fatalf("expected ts.Between(%v, %v) to return %v; instead got %v", a, b, ts[1:2], x)
+	y1 := ts.Ys[1]
+	y2 := ts.Ys[2]
+	middle := ts.Slice(1, 2)
+	if x := ts.Between(y1, y2); !x.Equal(middle) {
+		t.Fatalf("expected ts.Between(%v, %v) to return %v; instead got %v", y1, y2, middle, x)
 	}
 }
 
@@ -105,19 +106,18 @@ func TestDifference(t *testing.T) {
 	}
 
 	ts := Timeseries{
-		Point{time.Date(2018, 2, 15, 22, 41, 31, 0, time.UTC), 100.0},
-		Point{time.Date(2018, 2, 15, 22, 41, 41, 0, time.UTC), 50.0},
-		Point{time.Date(2018, 2, 15, 22, 41, 51, 0, time.UTC), 100.0},
+		Xs: []float64{100.0, 50.0, 100.0},
+		Ys: []float64{1, 2, 3},
 	}
 
-	if x := ts[0:1].Difference(); !x.Equal(emptyTimeseries) {
+	if x := ts.Slice(1, 2).Difference(); !x.Equal(emptyTimeseries) {
 		t.Fatalf("expected difference of 1-length series to return empty series; instead got %v", x)
 	}
 
 	actual := ts.Difference()
 	expected := Timeseries{
-		Point{ts[1].Time, -50.0},
-		Point{ts[2].Time, 50.0},
+		Xs: []float64{-50.0, 50.0},
+		Ys: []float64{2, 3},
 	}
 
 	if !actual.Equal(expected) {
